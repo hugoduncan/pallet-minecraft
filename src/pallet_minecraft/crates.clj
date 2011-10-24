802c378c67eec4ccc8fee5bd21f44d8ffb6017df
Leonard Axelsson <leo@xlson.com>
2011-10-24 13:53:52 +0200

    fixed automated admin user

--- src/pallet_minecraft/crates.clj
+++ src/pallet_minecraft/crates.clj
@@ -1,8 +1,8 @@
 (ns pallet-minecraft.crates
   (:require
    [pallet.stevedore :as stevedore]
-   [pallet.crate.atuomated-admin-user :as automated-admin-user]))
+   [pallet.crate.automated-admin-user :as automated-admin-user]))
 
 (defn bootstrap
   [req]
-  (automated-admin-user/automated-admin-user))
+  (automated-admin-user/automated-admin-user req))
