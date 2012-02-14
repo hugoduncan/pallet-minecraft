33c70328d44af941f2e2d50b0efae2f297607087
Hugo Duncan <hugo@hugoduncan.org>
2012-02-14 17:17:02 -0500

    Update for pallet 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject pallet-minecraft "1.0.0-SNAPSHOT"
   :description "minecraft with pallet"
-  :dependencies [[org.cloudhoist/pallet "0.6.7"]
-                 [org.cloudhoist/pallet-crates-all "0.5.0"]
-                 [org.jclouds.provider/serverlove-z1-man "1.2.1"]
-                 [org.jclouds.provider/aws-ec2 "1.2.1"]
-                 [org.jclouds.provider/aws-s3 "1.2.1"]
-                 [org.jclouds/jclouds-core "1.2.1"]
-                 [org.jclouds/jclouds-compute "1.2.1"]
-                 [org.jclouds/jclouds-blobstore "1.2.1"]
-                 [org.jclouds.driver/jclouds-log4j "1.2.1"]
-                 [log4j/log4j "1.2.14"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"] ; swank
-                     [org.cloudhoist/pallet-lein "0.4.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0-lc"]
+                 [org.cloudhoist/pallet "0.7.0-SNAPSHOT"]
+                 [org.cloudhoist/pallet-jclouds "1.4.0-SNAPSHOT"]
+                 [org.cloudhoist/pallet-vmfest "0.2.0-SNAPSHOT"]
+                 [org.cloudhoist/java "0.5.1"]
+                 [org.slf4j/slf4j-api "1.6.1"]
+                 [ch.qos.logback/logback-core "1.0.0"]
+                 [ch.qos.logback/logback-classic "1.0.0"]
+                 [org.jclouds.provider/aws-ec2 "1.4.0-rc.1"]
+                 [org.jclouds.driver/jclouds-slf4j "1.4.0-rc.1"]
+                 [org.jclouds.driver/jclouds-jsch "1.4.0-rc.1"]]
+  :dev-dependencies [[org.cloudhoist/pallet-lein "0.4.1"]]
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases"
                  "sonatype-snapshots"
